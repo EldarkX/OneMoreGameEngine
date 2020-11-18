6f5e41688b8d0d21118cb15d9fcca95ec25d0576
EldarkX <andrey.antropov96@gmail.com>
2020-11-19 00:54:33 +0200

    fix anim sprite component

--- Private/Modules/ObjectModule/Object/Actor/Components/AnimSpriteComponent.cpp
+++ Private/Modules/ObjectModule/Object/Actor/Components/AnimSpriteComponent.cpp
@@ -1,6 +1,17 @@
 
 #include "Modules/ObjectModule/Object/Actor/Components/AnimSpriteComponent.h"
 
+#include "Modules/ObjectModule/Object/Actor/Actor.h"
+
+void AnimSpriteComponent::SetTextures(const vector<SDL_Texture*>& Textures)
+{
+	mTextures = Textures;
+
+	mCurrentFrame = 0;
+
+	mTexturesAmount = mTextures.size();
+}
+
 void AnimSpriteComponent::Tick(float deltaTime)
 {
 	if (mTexturesAmount > 0)
@@ -14,4 +25,9 @@ void AnimSpriteComponent::Tick(float deltaTime)
 
 		SetTexture(mTextures[static_cast<int>(mCurrentFrame)]);
 	}
+}
+
+void AnimSpriteComponent::BeginPlay()
+{
+	SpriteComponent::BeginPlay();
 }
--- Public/Modules/ObjectModule/Object/Actor/Components/AnimSpriteComponent.h
+++ Public/Modules/ObjectModule/Object/Actor/Components/AnimSpriteComponent.h
@@ -8,24 +8,19 @@ class AnimSpriteComponent : public SpriteComponent
 public:
 
 	vector<SDL_Texture*>			GetTextures() const { return mTextures; }
-	void							SetTextures(const vector<SDL_Texture*>& Textures)
-	{
-		mTextures.clear();
-		for (SDL_Texture* texture : Textures)
-		{
-			mTextures.push_back(texture);
-		}
-		mTexturesAmount = mTextures.size();
-	}
+	void							SetTextures(const vector<SDL_Texture*>& Textures);
 	
 	int								GetAnimFPS() const { return mAnimFps; }
 	void							SetAnimFPS(int AnimFPS) { mAnimFps = AnimFPS; }
 
 	virtual void					Tick(float deltaTime) override;
 
+
+	virtual void BeginPlay() override;
+
 protected:
 
-	vector<SDL_Texture*>			mTextures;
+	vector<SDL_Texture *>			mTextures;
 	float							mCurrentFrame = 0;
 	unsigned int					mAnimFps = 24;
 
